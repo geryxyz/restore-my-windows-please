dfaa4ba5609a437b33d75670ce5c862e36ecbde6
Gergő Balogh <geryxyz@gmail.com>
2024-10-02 21:01:01 +0200

    feat: reading declaration for restore but not restoring anything yet

--- src/geryxyz/restore_my_windows_please/restore_please.py
+++ src/geryxyz/restore_my_windows_please/restore_please.py
@@ -4,17 +4,23 @@ from pathlib import Path
 
 import structlog
 
+from geryxyz.restore_my_windows_please.model.location import LocationDeclaration
+
 logger = structlog.stdlib.get_logger()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Restore your windows")
-    parser.add_argument("--preset", type=Path, help="Preset to use")
+    parser.add_argument("preset", type=Path, help="Preset to use")
     args = parser.parse_args()
 
     logger.info("Restoring windows", preset=args.preset)
-    preset_file: Path = args.preset
-    entries = json.loads(preset_file.read_text())
+    with open(args.preset, "r") as preset_file:
+        entries = json.load(preset_file)
+        entries = [LocationDeclaration(**entry) for entry in entries]
+
+    for entry in entries:
+        entry.restore()
 
 
 if __name__ == "__main__":
