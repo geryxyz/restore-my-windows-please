5d42c52e68c61dec69a8aa9c418988e073e5c7f5
Gergő Balogh <geryxyz@gmail.com>
2024-10-03 20:17:00 +0200

    feat: restoring size and position working based on tries

--- src/geryxyz/restore_my_windows_please/model/location.py
+++ src/geryxyz/restore_my_windows_please/model/location.py
@@ -1,3 +1,6 @@
+import re
+
+from ahk import AHK
 from pydantic import BaseModel
 
 
@@ -16,4 +19,13 @@ class LocationDeclaration(BaseModel):
     @property
     def unique_id(self) -> tuple[str, str]:
         return self.process_regex, self.window_title_regex
-    
+
+    def restore(self):
+        ahk = AHK()
+        windows = ahk.list_windows()
+        for window in windows:
+            title_match = re.search(self.window_title_regex, window.title)
+            process_match = re.search(self.process_regex, window.process_name)
+            if title_match and process_match:
+                window.move(self.left, self.top, width=self.width, height=self.height)
+                window.activate()
