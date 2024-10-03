53e0e9e1ccdb6e64f84c09c32f021d5cae458f7f
Gergő Balogh <geryxyz@gmail.com>
2024-10-03 20:54:34 +0200

    feat: restoring state (max min normal) but do not save minimalized windows

--- src/geryxyz/restore_my_windows_please/model/location.py
+++ src/geryxyz/restore_my_windows_please/model/location.py
@@ -1,7 +1,19 @@
 import re
+from enum import StrEnum, IntEnum
+from time import sleep
 
+import structlog
 from ahk import AHK
 from pydantic import BaseModel
+
+
+logger = structlog.stdlib.get_logger()
+
+
+class WindowState(IntEnum):
+    MINIMIZED = -1
+    MAXIMIZED = 1
+    RESTORED = 0
 
 
 class LocationDeclaration(BaseModel):
@@ -11,6 +23,8 @@ class LocationDeclaration(BaseModel):
     left: int = 0
     width: int = 100
     height: int = 100
+    state: WindowState | None = None
+
     
     class Config:
         frozen = True
@@ -27,5 +41,13 @@ class LocationDeclaration(BaseModel):
             title_match = re.search(self.window_title_regex, window.title)
             process_match = re.search(self.process_regex, window.process_name)
             if title_match and process_match:
-                window.move(self.left, self.top, width=self.width, height=self.height)
-                window.activate()
+                if self.state == WindowState.MAXIMIZED:
+                    window.maximize()
+                    logger.info("Maximized window", window=window.title, process=window.process_name)
+                elif self.state == WindowState.MINIMIZED:
+                    window.minimize()
+                    logger.info("Minimized window", window=window.title, process=window.process_name)
+                else:
+                    window.move(self.left, self.top, width=self.width, height=self.height)
+                    window.restore()
+                    logger.info("Restored window", window=window.title, process=window.process_name)
--- src/geryxyz/restore_my_windows_please/save_please.py
+++ src/geryxyz/restore_my_windows_please/save_please.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import structlog
 from ahk import AHK
 
-from geryxyz.restore_my_windows_please.model.location import LocationDeclaration
+from geryxyz.restore_my_windows_please.model.location import LocationDeclaration, WindowState
 
 logger = structlog.stdlib.get_logger()
 
@@ -21,15 +21,22 @@ def main():
     entries = dict()
     for window in windows:
         position = window.get_position()
-        declaratoin = LocationDeclaration(
+        minmax_state = window.get_minmax()
+        state = WindowState(int(minmax_state))
+        if state == WindowState.MINIMIZED:
+            logger.warn("Skipping minimized window", window=window.title, process=window.process_name)
+            continue
+        declaration = LocationDeclaration(
             process_regex=window.process_name,
             window_title_regex=window.title,
             top=position.y,
             left=position.x,
             width=position.width,
             height=position.height,
+            state=state
         )
-        entries[declaratoin.unique_id] = declaratoin
+        entries[declaration.unique_id] = declaration
+        logger.info("Saved window", window=window.title, process=window.process_name)
     with open(args.preset, "w") as preset_file:
         preset_file.write(json.dumps([value.model_dump() for value in entries.values()], indent=4))
 
